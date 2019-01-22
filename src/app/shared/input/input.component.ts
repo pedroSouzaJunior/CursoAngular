4721524f91324c054d0806b2d42efb4871919532
Pedro <pedro.souza.psj@gmail.com>
2019-01-21 23:54:27 -0300

    novo input

---
+++ src/app/shared/input/input.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input, ContentChild, AfterContentInit } from '@angular/core';
+import { NgModel } from '@angular/forms';
+
+@Component({
+  selector: 'mt-input-container',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css']
+})
+export class InputComponent implements OnInit, AfterContentInit {
+  input: any;
+  @Input() label: string;
+  @Input() errorMessage: string;
+
+  @ContentChild(NgModel) model: NgModel;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  ngAfterContentInit(): void {
+    this.input = this.model;
+    if (this.input === undefined){
+      throw new Error('Esse componente precisa ser usadom com a diretiva ngModel');
+    }
+  }
+
+
+}
